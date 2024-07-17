552041717b914f461c8f810cba6f6599e0a109cf
shufanfan <chai.shu@northeastern.edu>
2024-07-17 15:21:05 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+Lab5(app);
+
+Hello(app);
+
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+
+app.listen(process.env.PORT || 4000);
